70bb74e0d344db0f9f44220afbb24ea9c749af75
Lukman Hakim <zyanlilou@gmail.com>
2023-07-18 18:39:37 +0700

    CRUD create

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -8,6 +8,13 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+    {{-- trix editor --}}
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+    <style>
+        trix-toolbar [data-trix-button-group="file-tools"] {
+            display: none;
+        }
+    </style>
   </head>
   <body>
 
@@ -24,6 +31,9 @@
         </div>
     </div>
 
+    {{-- trix editor  --}}
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+    {{-- bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/feather-icons"></script>
     <script>feather.replace()</script>
--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -5,13 +5,52 @@
     <div class="row justify-content-center">
         <div class="col-lg-8">
             <form action="/dashboard/posts" method="POST">
+                @csrf
                 <div class="mt-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Title in here">
+                    <input type="text" class="form-control @error('title')
+                    is-invalid
+                    @enderror" id="title" name="title" placeholder="Title in here" autofocus value="{{ old('title') }}">
+                    @error('title')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="mt-3">
                     <label for="slug" class="form-label">Slug</label>
-                    <input type="text" class="form-control" id="slug" name="slug" placeholder="Title in here">
+                    <input type="text" class="form-control @error('slug')
+                        is-invalid
+                    @enderror" id="slug" name="slug" placeholder="Title in here" disabled readonly value="{{ old('slug') }}">
+                    @error('slug')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="mt-3">
+                    <label for="category" class="form-label">Category</label>
+                    <select name="category_id" id="" class="form-select">
+                        @foreach ($categories as $itemCategory)
+                            @if (old('category_id') == $itemCategory->id)
+                                <option value="{{ $itemCategory->id }}" selected>{{ $itemCategory->name }}</option>
+                            @else
+                            <option value="{{ $itemCategory->id }}">{{ $itemCategory->name }}</option>
+                        @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mt-3">
+                    <label for="body" class="form-body @error('body')
+                        is-invalid
+                    @enderror">Body</label>
+                    @error('body')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                    <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+                    <trix-editor input="body"></trix-editor>
                 </div>
 
                 <button type="submit" class="btn btn-primary mt-3">Created Post</button>
@@ -19,4 +58,19 @@
         </div>
     </div>
 </div>
+
+<script>
+    const title = document.querySelector('#title');
+    const slug = document.querySelector('#slug');
+
+    title.addEventListener('change', function() {
+        fetch('/dashboard/posts/checkSlug?title='+ title.value)
+        .then(response => response.json())
+        .then(data => slug.value = data.slug)
+    });
+
+    document.addEventListener('trix-file-accept', function(e) {
+        e.preventDefault();
+    })
+</script>
 @endsection
