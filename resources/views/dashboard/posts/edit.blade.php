ae9b295e98ac429ca8264ab658763d4d88726ff9
Lukman Hakim <zyanlilou@gmail.com>
2023-07-21 09:12:37 +0700

    Delete and Update

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -2,19 +2,20 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Cretae Post</h1>
+    <h1 class="h2">Edit Post</h1>
 </div>
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <form action="/dashboard/posts" method="POST">
+            <form action="/dashboard/posts/{{ $post->slug }}" class="mb-5" method="POST">
+                @method('put')
                 @csrf
                 <div class="mt-3">
                     <label for="title" class="form-label">Title</label>
                     <input type="text" class="form-control @error('title')
                     is-invalid
-                    @enderror" id="title" name="title" placeholder="Title in here" autofocus value="{{ old('title') }}">
+                    @enderror" id="title" name="title" placeholder="Title in here" autofocus value="{{ old('title', $post->title) }}">
                     @error('title')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -25,7 +26,7 @@
                     <label for="slug" class="form-label">Slug</label>
                     <input type="text" class="form-control @error('slug')
                         is-invalid
-                    @enderror" id="slug" name="slug" placeholder="Title in here" disabled readonly value="{{ old('slug') }}">
+                    @enderror" id="slug" name="slug" placeholder="Title in here" disabled readonly value="{{ old('slug', $post->slug) }}">
                     @error('slug')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -36,7 +37,7 @@
                     <label for="category" class="form-label">Category</label>
                     <select name="category_id" id="" class="form-select">
                         @foreach ($categories as $itemCategory)
-                            @if (old('category_id') == $itemCategory->id)
+                            @if (old('category_id', $post->category_id) == $itemCategory->id)
                                 <option value="{{ $itemCategory->id }}" selected>{{ $itemCategory->name }}</option>
                             @else
                             <option value="{{ $itemCategory->id }}">{{ $itemCategory->name }}</option>
@@ -53,11 +54,11 @@
                             {{ $message }}
                         </div>
                     @enderror
-                    <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+                    <input id="body" type="hidden" name="body" value="{{ old('body', $post->body) }}">
                     <trix-editor input="body"></trix-editor>
                 </div>
 
-                <button type="submit" class="btn btn-primary mt-3">Created Post</button>
+                <button type="submit" class="btn btn-primary mt-3">Update Post</button>
             </form>
         </div>
     </div>
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -5,6 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-lg-8">
             <h1>{{ $posts->title }}</h1>
+            <button type="button" class="btn btn-success">Back to all my posts</button>
+            <a href="/dashboard/posts/{{ $posts->slug }}/edit" class="btn btn-warning">Edit</a>
+            <form action="/dashboard/posts/{{ $posts->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button onclick="return confirm('are you sure ?')" class="btn btn-danger">Delete</button>
+            </form>
             <p>
                 By. <a href="/posts?author={{ $posts->author->username }}" class="text-decoration-none"> {{ $posts->author->name }}</a>
                 in <a href="/posts?category={{ $posts->category->slug }}" class="text-decoration-none">{{ $posts->category->name }}</a>
